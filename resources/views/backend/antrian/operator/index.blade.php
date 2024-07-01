236b771d429a4d42b98dae45b27b18548b08011b
iticbackup <ict.indonesiantobacco.backup@gmail.com>
2024-07-01 10:29:32 +0700

    Portal HRD

--- resources/views/backend/antrian/operator/index.blade.php
+++ resources/views/backend/antrian/operator/index.blade.php
@@ -41,7 +41,7 @@
                     @endif
                     <button class="btn btn-info mb-2 me-2" onclick="window.location.href='{{ route('antrian') }}'">
                         <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 28 28">
-                            <path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20 11A8.1 8.1 0 0 0 4.5 9M4 5v4h4m-4 4a8.1 8.1 0 0 0 15.5 2m.5 4v-4h-4" />
+                            <path fill="currentColor" d="M19 12.998h-6v6h-2v-6H5v-2h6v-6h2v6h6z" />
                         </svg> Buat Baru
                     </button>
                     <button class="btn btn-info mb-2 me-4" onclick="reload()">Reload</button>
