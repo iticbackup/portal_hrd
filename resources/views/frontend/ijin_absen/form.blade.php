199bd5838d4290975b73a569a184b8d2eefde573
iticbackup <ict.indonesiantobacco.backup@gmail.com>
2024-10-17 08:20:53 +0700

    Portal HRD

--- resources/views/frontend/ijin_absen/form.blade.php
+++ resources/views/frontend/ijin_absen/form.blade.php
@@ -103,7 +103,8 @@
                                             <label>Departemen</label>
                                             <input type="text" name="departemen" class="form-control"
                                                 placeholder="Departemen"
-                                                value="{{ !auth()->user()->biodata_karyawan ? null : auth()->user()->biodata_karyawan->departemen->nama_departemen <= 11 ? auth()->user()->biodata_karyawan->departemen->nama_unit : '-' }}"
+                                                {{-- value="{{ auth()->user()->biodata_karyawan->departemen }}" --}}
+                                                value="{{ !auth()->user()->biodata_karyawan ? null : auth()->user()->biodata_karyawan->departemen->nama_departemen >= 2 ? auth()->user()->biodata_karyawan->departemen->nama_unit : auth()->user()->biodata_karyawan->departemen->nama_departemen }}"
                                                 readonly id="departemen" style="color: black">
                                         </div>
                                     </div>
